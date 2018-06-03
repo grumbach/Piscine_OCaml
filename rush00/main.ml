b938d314191cd8ae02637daf6631054d8f802a8d
dkrm <git@dkrm.fastmail.com>
2018-06-03 18:41:29 +0200

    [ADD] we did it boyz!

--- rush00/main.ml
+++ rush00/main.ml
@@ -28,7 +28,25 @@ let rec handle_player_turn board player =
      end
   | Board.Pair (x, y) when Board.is_move_available board (Board.coordinate_proj x) (Board.coordinate_proj y)
     ->
-     Board.add_player_move board (Board.coordinate_proj x) (Board.coordinate_proj y) player
+     let coor_x = (Board.coordinate_proj x)
+     and coor_y = (Board.coordinate_proj y)
+     in
+     let new_board = Board.add_player_move board coor_x coor_y player
+     in
+     let cell = Board.get_cell_from_coordinates new_board coor_x coor_y
+     and cell_nb = Board.get_cell_number_with coor_x coor_y
+     in
+     let winning_symbol = Board.resolve cell
+     in
+     if winning_symbol <> Board.None
+     then
+       begin
+         print_string ((Board.str_of_sym player) ^ " wins grid ");
+         print_int cell_nb;
+         print_endline "!\n";
+         new_board
+       end
+     else new_board
   | _
     ->
      begin
@@ -40,13 +58,19 @@ let rec handle_player_turn board player =
 let do_player_turn board player =
   begin
     Board.display_board board;
-    print_endline ((Board.str_of_sym player) ^ "'s turn to play.");
+    print_endline ("\n" ^ (Board.str_of_sym player) ^ "'s turn to play.");
     handle_player_turn board player
   end
 
 let rec main_loop board player =
-  if Board.is_there_a_winner board
-  then Board.display_board board
+  let winner = Board.is_there_a_winner board
+  in
+  if winner <> Board.None
+  then
+    begin
+      print_endline ((Board.str_of_sym player) ^ " wins the game!\n");
+      Board.display_board board
+    end
   else
     match player with
 	| Board.X | Board.None -> main_loop (do_player_turn board Board.O) Board.O
